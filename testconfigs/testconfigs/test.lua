560fe0e7037a258549efd1607bc2970ea361f40f
Ernie <ernestohh97@gmail.com>
2025-05-30 14:57:48 +0200

    Add files via upload

---
+++ testconfigs/testconfigs/test.lua
@@ -0,0 +1,15 @@
+-- main.lua
+local API = require("api")
+
+-- CONFIG is automatically injected when user has configured the script
+if CONFIG then
+    print("\n--- All CONFIG keys and values ---")
+    for key, value in pairs(CONFIG) do
+        print(tostring(key) .. ": " .. tostring(value))
+    end
+    print("---------------------------------")
+end
+
+while (API.Read_LoopyLoop()) do
+
+end
