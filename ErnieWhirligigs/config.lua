e2cb8fd75762cc1159d34da3e313753683b777c3
Ernie <ernestohh97@gmail.com>
2025-10-01 21:17:06 +0200

    ErnieWhirligigs
    fast fast fast

---
+++ ErnieWhirligigs/config.lua
@@ -0,0 +1,5 @@
+-- Ernie Whirligigs Configuration
+SM:AddTab("Whirligig Settings")
+SM:Dropdown("Initial whirligig", "initialWhirligig", {"Plain whirligig", "Dazzling whirligig", "Gliding whirligig", "Swift whriligig", "Hasty whirligig", "Speedy whirligig"})
+SM:Dropdown("Stacking whirligig", "stackingWhirligig", {"Plain whirligig", "Dazzling whirligig", "Gliding whirligig", "Swift whriligig", "Hasty whirligig", "Speedy whirligig"})
+SM:Checkbox("Increased scarab stacks unlocked", "increasedStackingAmount", false)
